bdae9d1df90a164f2bea2beafcd5963cd67a5dd4
Gustavo Seganfredo <gustavosf@gmail.com>
2012-03-08 19:04:02 -0300

    Fresh rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bolao::Application.config.secret_token = '8e92d5125cf1a5e940713fa867f6e22f361a2b4c6a82ce035313b532d1db35c65978e860c5e1585e6c07ec157a41891d0f3c5a251d146c0f84c13f02b716f4f4'
