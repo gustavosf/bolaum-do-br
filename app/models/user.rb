5c12b970b2901745e2d0200ca1bd40e8b55573fa
Gustavo Seganfredo <gustavosf@gmail.com>
2012-03-08 20:02:17 -0300

    Simple authentication created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  #attr_accessible :email, :password_digest, :name, :last_login
+
+  validates_presence_of :email
+  validates_presence_of :name
+  validates_presence_of :password, :on => :create
+
+  validates_uniqueness_of :email
 end
